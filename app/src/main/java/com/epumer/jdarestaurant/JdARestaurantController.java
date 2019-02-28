aa9b028a688cd8b070d2a6260903b0c54283b13a
Epumer Pehuen Iribarren Inchausti <eiribarren@iesjoandaustria.org>
2019-02-28 18:00:04 +0100

    Initial commit

---
+++ app/src/main/java/com/epumer/jdarestaurant/JdARestaurantController.java
@@ -0,0 +1,13 @@
+package com.epumer.jdarestaurant;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class JdARestaurantController extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_jda_restaurant_controller);
+    }
+}
